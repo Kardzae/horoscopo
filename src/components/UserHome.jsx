10935501cba865ec282d784abf743e66a2c2a898
kardzae <emmanuelpp21@gmail.com>
2023-09-12 13:40:29 -0500

    actualizando repositorio

--- src/components/UserHome.jsx
+++ src/components/UserHome.jsx
@@ -1,12 +1,41 @@
 import { Navigate } from "react-router-dom";
+import './styles/UserHome.css';
+import TextSigno from "./TextSigno.jsx";
+import { useState } from "react";
 
 function UserHome({user}){
-    if(user!=="user"){
+    if(user!=="user" || !user){
         return <Navigate to="/"/>
     }
+    const [textoSigno, setTextoSigno] = useState('');
+
+    async function handleSelect(event){
+        const signo = event.target.value;
+        if(signo!=="0"){
+            fetch(`http://localhost:4000/v1/signos/${signo}`)
+                .then(response => response.json())
+                .then(responseData => setTextoSigno(responseData))
+        } 
+    }
+
     return (
-        <div>
-            <h2>User Home (Private)</h2>
+        <div className="container">
+            <div id="txtSeleccionPage"><h3>Selecciona tu signo zodiacal</h3></div>
+            <select id="selectSignos" onClick={handleSelect}>
+                <option value="0">Seleciona un signo zodiacal</option>
+                <option value="Aries">Aries</option>
+                <option value="Geminis">Géminis</option>
+                <option value="Cancer">Cáncer</option>
+                <option value="Leo">Leo</option>
+                <option value="Virgo">Virgo</option>
+                <option value="Libra">Libra</option>
+                <option value="Escorpio">Escorpio</option>
+                <option value="Sagitario">Sagitario</option>
+                <option value="Capricornio">Capricornio</option>
+                <option value="Acuario">Acuario</option>
+                <option value="Piscis">Piscis</option>
+            </select>
+            <TextSigno texto={textoSigno}/>
         </div>
     )
 }
